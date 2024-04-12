81c5b0bcbd517fdd99d4e05a1026b35f52dee1eb
Abhishek Singhria <abhisinghria@gmail.com>
2024-04-12 12:41:00 -0400

    projects modified

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,32 +1,34 @@
 import { FaProjectDiagram } from "react-icons/fa";
 import Title from "./Title";
-import KaneImage from "../public/img/projects/Kane.jpeg";
-import WeatherAppImage from "../public/img/projects/weather-app-image.png";
-import BitcoinImage from "../public/img/projects/bitcoin.jpg"; // Import the Bitcoin Analysis project image
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
+  // Import the images directly in the Projects component
+  const KaneImage = "/img/projects/Kane.jpeg";
+  const WeatherAppImage = "/img/projects/weather-app-image.png";
+  const BitcoinImage = "/img/projects/bitcoin.jpg";
+
   return (
     <div className="wrapper">
       <Title text="Projects" icon={<FaProjectDiagram />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
         {/* KANE Project */}
         <ProjectCard
-          imgSrc={KaneImage}
+          imgSrc={KaneImage} // Pass the image path as a string
           title="KANE"
           link="https://kaneproject.ca/"
         />
         
         {/* Weather App */}
         <ProjectCard
-          imgSrc={WeatherAppImage}
+          imgSrc={WeatherAppImage} // Pass the image path as a string
           title="Weather App"
           link="https://github.com/AbhishekSinghria/Weather-App"
         />
 
         {/* Bitcoin Analysis Project */}
         <ProjectCard
-          imgSrc={BitcoinImage}
+          imgSrc={BitcoinImage} // Pass the image path as a string
           title="Bitcoin Analysis"
           link="https://github.com/AbhishekSinghria/Bitcoin_Analysis_Data/tree/master"
         />
